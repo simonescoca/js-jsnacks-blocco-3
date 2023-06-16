ed14e62f39573ad47bb86a6c4f223b55950fe0b7
simonescoca <simone.scoca@icloud.com>
2023-06-16 12:15:38 +0200

    added to App.vue the AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
-<script setup>
-import AppHeader from './components/AppHeader.vue'
+<script>
+    // Usando il progetto creato ieri in classe, vue3-header, aggiungere un componente figlio di AppHeader,
+    // che si chiami NavLink, istanziare tanti navlink quanti link sono presenti nella navbar e
+    // passare il suo nome come prop da AppHeader a NavLink
+    import AppHeader from './components/AppHeader.vue'
+    export default {
+        components: {
+            AppHeader
+        },
+        data() {
+            return {
+
+            }
+        }
+    }
 </script>
 
 <template>
+    <AppHeader />
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 </style>
