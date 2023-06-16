0d2bacf865b70c8c6a6c4d7a1d1ca9de14ce24f9
simonescoca <simone.scoca@icloud.com>
2023-06-16 12:16:01 +0200

    added top AppHeader the NavLink component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,20 @@
 <script>
-    
+    import NavLink from './NavLink.vue'
+    export default {
+        components: {
+            NavLink
+        },
+        data() {
+            return {
+
+            }
+        }
+    }
 </script>
 
 <template>
-
+    <NavLink />
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 </style>
